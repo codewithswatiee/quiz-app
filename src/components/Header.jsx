e2667fe3635db26316c000973913f63d4ae8c877
Swati Verma <121037311+codewithswatiee@users.noreply.github.com>
2024-06-23 05:47:27 +0000

    commit 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo  from '../assets/quiz-logo.png'
+function Header() {
+  return (
+    <header>
+        <img src={logo}></img>
+        <h1>
+            React Quiz
+        </h1>
+    </header>
+  )
+}
+
+export default Header
